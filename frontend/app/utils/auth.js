2976af9834efc2ab66243ea51f9d605ad0d8a440
Sowmia Mary <tomailsr@gmail.com>
2025-11-19 17:09:37 +0400

    gfgh

--- frontend/app/utils/auth.js
+++ frontend/app/utils/auth.js
@@ -12,11 +12,11 @@ export function AuthProvider({ children }) {
   );
 
   const login = async (email, password) => {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
 
     const data = await res.json();
 
